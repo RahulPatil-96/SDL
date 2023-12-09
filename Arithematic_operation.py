65357b906f6de38adc999ef60d6c7e6084413216
Rahul Vijay Patil <143159214+RahulPatil-96@users.noreply.github.com>
2023-12-09 23:48:49 +0530

    Create Arithematic_operation.py

---
+++ Arithematic_operation.py
@@ -0,0 +1,36 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print("Select the operation you want to perform:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice: ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if choice == '1':
+    result = add(num1, num2)
+elif choice == '2':
+    result = subtract(num1, num2)
+elif choice == '3':
+    result = multiply(num1, num2)
+elif choice == '4':
+    result = divide(num1, num2)
+else:
+    print("Invalid input. Please enter a valid choice.")
+    exit()
+
+print("The result is: ", result)     
